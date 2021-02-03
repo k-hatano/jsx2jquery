8398334048e56e1c9cc6f6e660c8a266f2d855da
k-hatano <kent.ruffle.mgj626@gmail.com>
2021-02-04 02:20:45 +0900

    サンプル内容更新

--- jsx.jsx
+++ jsx.jsx
@@ -1,32 +1,24 @@
-
-function getDiv1() {
-	return <View>Test</View>;
+function sample1(){
+  return (
+    <View>
+      <span>first content</span>
+      <span>second content</span>
+    </View>
+  );
 }
 
-function getDiv2() {
-	let content = "";
-	for (i = 0; i < 5; i++) {
-		content += <View><span>{i + 1}</span></View>;
-	}
-	return content;
-}
-
-function getDiv3() {
-	return (
-		<View>
-			<span>This is the first content.</span>
-			<span>This is the second content.</span>
-		</View>
-	);
-}
-
-function getDiv4() {
-	let array = [1, 2, 3, 4, 5];
-	return (
-			<View>
-				<div>
-					{ array.map(i => <View><span>Item {i}</span></View>).join('') }
-				</div>
-			</View>
-		);
+function sample2(){
+  let arr = [1, 2, 3, 4, 5];
+  return (
+    <View>
+      <span>Title</span>
+      <div>
+        { arr.map(i => 
+          <View>
+            <span>content {i}</span>
+          </View>
+        ).join('') }
+      </div>
+    </View>
+  );
 }
